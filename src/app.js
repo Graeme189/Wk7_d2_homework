4206e17a5ebf16f367a0c56e9392d59101f4896b
Graeme Henderson <graeme.189@gmail.com>
2019-08-06 18:45:08 +0100

    Converter to Euros working and reduced to two decimal places

--- src/app.js
+++ src/app.js
@@ -2,16 +2,33 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue ({
-    el: "#app"
+    el: "#app",
     data: {
-      currencies: []
+      rateObjects: null,
+      base: null,
+      chosenRate: null,
+      valueToConvert: null,
+
     }, // data brackets
+    mounted() {
+      this.fetchCurrencies();
+    },
+    computed: {
+        convertFromEuros: function () {
+        const formattedAmount = this.valueToConvert * this.chosenRate;
+          return formattedAmount.toFixed(2)
+        }
+    },
     methods: {
       fetchCurrencies: function () {
-        const request = fetch("https://api.exchangeratesapi.io/latest")
+        fetch("https://api.exchangeratesapi.io/latest")
         .then(response => response.json())
-        .then(fetchedCurrencies => this.currencies = fetchedCurrencies)
-      } //fetchCurrencies brackets
+        .then((data) => {
+          console.log(data);
+          this.rateObjects = data.rates
+          this.base = data.base
+        });
+      }, //fetchCurrencies brackets
     } // methods brackets
   }) // new Vue brackets
 }) // document brackets
